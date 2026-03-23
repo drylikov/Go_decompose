074d3967586196d2d58dffacd82d30c2215ac6c3
drylikov <denis.rylikov@protonmail.com>
2026-03-23 11:45:03 +0300

    Go_decompose
    Package decompose lets you arbitrarily de-nest JSON configuration into multiple files.

---
+++ decompose.go
@@ -0,0 +1,75 @@
+
+// Package decompose lets you arbitrarily de-nest JSON configuration into multiple files.
+package decompose
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Decompose starting at `root` path, returning a JSON blob. If `root` is a JSON file itself
+// and not a directory, its contents is simply returned.
+func Decompose(root string) ([]byte, error) {
+	m := make(map[string]interface{})
+
+	// TODO: actually support arbitrary depths :)
+
+	info, err := os.Stat(root)
+	if err != nil {
+		return nil, err
+	}
+
+	if info.Mode().IsRegular() {
+		return ioutil.ReadFile(root)
+	}
+
+	err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		base, err := filepath.Rel(root, path)
+		if err != nil {
+			return err
+		}
+
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+
+		if !strings.HasSuffix(path, ".json") {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		var v map[string]interface{}
+		if err := json.NewDecoder(f).Decode(&v); err != nil {
+			return err
+		}
+
+		name := strings.Replace(base, ".json", "", -1)
+
+		if "index" == name {
+			for k, v := range v {
+				m[k] = v
+			}
+			return nil
+		}
+
+		m[name] = v
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return json.MarshalIndent(m, "", "  ")
+}
---
+++ decompose_test.go
@@ -0,0 +1,130 @@
+
+package decompose
+
+import (
+	"testing"
+)
+
+var result = `{
+  "definitions": {
+    "Error": {
+      "properties": {
+        "code": {
+          "format": "int32",
+          "type": "integer"
+        },
+        "message": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "code",
+        "message"
+      ],
+      "type": "object"
+    },
+    "Pet": {
+      "properties": {
+        "id": {
+          "format": "int64",
+          "type": "integer"
+        },
+        "name": {
+          "type": "string"
+        },
+        "tag": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "id",
+        "name"
+      ],
+      "type": "object"
+    }
+  },
+  "name": "api",
+  "paths": {
+    "/pets": {
+      "get": {
+        "responses": {
+          "200": {
+            "description": "pet response",
+            "headers": {
+              "x-expires": {
+                "type": "string"
+              }
+            },
+            "schema": {
+              "items": {
+                "$ref": "#/definitions/Pet"
+              },
+              "type": "array"
+            }
+          },
+          "default": {
+            "description": "unexpected error",
+            "schema": {
+              "$ref": "#/definitions/Error"
+            }
+          }
+        },
+        "summary": "finds pets in the system"
+      }
+    }
+  }
+}`
+
+var pathsResult = `{
+  "/pets": {
+    "get": {
+      "summary": "finds pets in the system",
+      "responses": {
+        "200": {
+          "description": "pet response",
+          "schema": {
+            "type": "array",
+            "items": {
+              "$ref": "#/definitions/Pet"
+            }
+          },
+          "headers": {
+            "x-expires": {
+              "type": "string"
+            }
+          }
+        },
+        "default": {
+          "description": "unexpected error",
+          "schema": {
+            "$ref": "#/definitions/Error"
+          }
+        }
+      }
+    }
+  }
+}`
+
+func TestDecompose_directory(t *testing.T) {
+	b, err := Decompose("_fixtures/gateway")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(b) != result {
+		t.Fatalf("got: `%s`", b)
+	}
+}
+
+func TestDecompose_singleFile(t *testing.T) {
+	b, err := Decompose("_fixtures/gateway/paths.json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(b) != pathsResult {
+		t.Fatalf("got: `%s`", b)
+	}
+}
